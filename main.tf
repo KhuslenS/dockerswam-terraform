87004274c59bfa988669412f018a48d97bb7fca4
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-05 20:28:52 -0500

    updated

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ resource "aws_instance" "master" {
   instance_type          = "${var.instance_type}"
   key_name               = "${var.key_name}"
   user_data              = "${file("${var.bootstrap_path}")}"
-  # user_data              = "${file("${var.manager_token}")}"
   vpc_security_group_ids = ["${aws_security_group.sgswarm.id}"]
 
 
@@ -18,10 +17,10 @@ resource "aws_instance" "master" {
   }
 }
 resource "aws_instance" "worker1" {
-  ami                    = "${var.ami}"
-  instance_type          = "${var.instance_type}"
-  key_name               = "${var.key_name}"
-  user_data              = "${file("${var.bootstrap_path}")}"
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name      = "${var.key_name}"
+  user_data     = "${file("${var.bootstrap_path}")}"
   # user_data              = "${file("${var.worker_token}")}"
   vpc_security_group_ids = ["${aws_security_group.sgswarm.id}"]
   tags = {
@@ -33,7 +32,6 @@ resource "aws_instance" "worker2" {
   instance_type          = "${var.instance_type}"
   key_name               = "${var.key_name}"
   user_data              = "${file("${var.bootstrap_path}")}"
-  user_data              = "${file("${var.worker_token}")}"
   vpc_security_group_ids = ["${aws_security_group.sgswarm.id}"]
   tags = {
     Name = "worker 2"
